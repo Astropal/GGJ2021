1c2cfaa60b15973b5292eb65ef0f18a77c5865db
Florian ROSSI <florian.rossi@viacesi.fr>
2021-01-30 18:48:09 +0100

    save

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickManager : MonoBehaviour {
+
+    public Object test;
+    private void OnMouseDown() {
+        TriggerObject(this.gameObject);
+    }
+
+        void TriggerObject(GameObject gameObject) 
+    {
+        Debug.Log(gameObject.name);
+        Debug.Log(test.name);
+    }
+
+}
---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ [System.Serializable]
+public class Object : MonoBehaviour
+{
+    public string name;
+    public bool toFound;
+
+    public bool isFound;
+}
