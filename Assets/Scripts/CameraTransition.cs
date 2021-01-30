c901e02ddf00c9e3eaca5eab83dfadd84d7a874f
Clément Albos <44971574+Xompre@users.noreply.github.com>
2021-01-30 13:30:57 +0100

    Animation Planets

---
+++ Assets/Scripts/CameraTransition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransition : MonoBehaviour
+{
+
+    public Camera mainCam;
+    public GameObject planet;
+
+    public string alreadyplayed = "n";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(planetClicked == "y"){
+            Vector3 desiredPosition = new Vector3(target.position.x, target.position.y, -100);
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
+            mainCam.transform.position = smoothedPosition;
+        }*/
+        
+    }
+
+
+    private void OnMouseDown() {
+
+        if(alreadyplayed == "n"){
+            string animationName = "CameraMove" + (string) planet.name;
+            mainCam.GetComponent<Animation>().Play(animationName);
+            alreadyplayed = "y";
+        } 
+        
+    }
+}
