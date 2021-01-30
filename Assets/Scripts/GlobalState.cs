3f5ca0a5fa6c9694fc216ae94d6eaeeba76f3b37
BaptisteMiq <baptiste.miquel@viacesi.fr>
2021-01-30 15:05:30 +0100

    Cannot click when gui opened

---
+++ Assets/Scripts/GlobalState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalState : MonoBehaviour {
+
+    public bool guiOpen = false;
+    public static GlobalState instance;
+    
+    void Awake(){
+        if (instance == null){
+            instance = this;
+            // DontDestroyOnLoad(this.gameObject);
+        } else {
+            Destroy(this);
+        }
+    }
+
+}
--- Assets/Scripts/MiniGame.cs
+++ Assets/Scripts/MiniGame.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class MiniGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    public void Show(bool state) {
-        gameObject.SetActive(state);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void Show(bool state)
+  {
+    if (!GlobalState.instance.guiOpen && state)
+    {
+      gameObject.SetActive(true);
+      GlobalState.instance.guiOpen = true;
     }
+    else if (GlobalState.instance.guiOpen && state)
+    {
+      return;
+    }
+    else
+    {
+      GlobalState.instance.guiOpen = false;
+      gameObject.SetActive(false);
+    }
+  }
 }
