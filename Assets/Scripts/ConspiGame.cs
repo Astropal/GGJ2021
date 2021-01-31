ca3b580c807ef453aedda761edda21b7a613f5c1
Clément Albos <44971574+Xompre@users.noreply.github.com>
2021-01-31 11:48:12 +0100

    MinijeuConspi

---
+++ Assets/Scripts/ConspiGame.cs
@@ -0,0 +1,101 @@
+﻿
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConspiGame : MonoBehaviour
+{
+    public int nbOutils = 0;
+
+    public GameObject cadreDialogue;
+
+    List<GameObject> listOfObject = new List<GameObject>(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(listOfObject.Count == 3){
+            Debug.Log("Bag Full !");
+            foreach (var item in listOfObject)
+            {
+                    Debug.Log(item.tag);
+                    
+            }
+            cadreDialogue.SetActive(true);
+
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D Col)
+    {
+        if(Col.gameObject.tag == "Arme"){
+           nbOutils++;
+           listOfObject.Add(Col.gameObject);
+           Debug.Log(nbOutils);
+        }
+        if(Col.gameObject.tag == "Ligth"){
+           nbOutils++;
+           listOfObject.Add(Col.gameObject);
+           Debug.Log(nbOutils);
+        }
+        if(Col.gameObject.tag == "Rope"){
+           nbOutils++;
+           listOfObject.Add(Col.gameObject);
+           Debug.Log(nbOutils);
+        }
+        if(Col.gameObject.tag == "Food"){
+           nbOutils++;
+           listOfObject.Add(Col.gameObject);
+           Debug.Log(nbOutils);
+        }
+        if(Col.gameObject.tag == "Camera"){
+           nbOutils++;
+           listOfObject.Add(Col.gameObject);
+           Debug.Log(nbOutils);
+        }
+
+
+
+    }
+    void OnTriggerExit2D(Collider2D Col)
+    {
+        if(Col.gameObject.tag == "Arme"){
+            nbOutils--;
+            listOfObject.Remove(Col.gameObject);
+            Debug.Log(nbOutils);
+        }
+        if(Col.gameObject.tag == "Ligth"){
+            nbOutils--;
+            listOfObject.Remove(Col.gameObject);
+            Debug.Log(nbOutils);
+        }
+        if(Col.gameObject.tag == "Rope"){
+            nbOutils--;
+            listOfObject.Remove(Col.gameObject);
+            Debug.Log(nbOutils);
+        }
+        if(Col.gameObject.tag == "Camera"){
+            nbOutils--;
+            listOfObject.Remove(Col.gameObject);
+            Debug.Log(nbOutils);
+        }
+        if(Col.gameObject.tag == "Food"){
+            nbOutils--;
+            listOfObject.Remove(Col.gameObject);
+            Debug.Log(nbOutils);
+        }
+    }
+}
---
+++ Assets/Scripts/DragTheObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragTheObject : MonoBehaviour
+{
+    public LayerMask m_DragLayers;
+
+    [Range (0.0f, 100.0f)]
+    public float m_Damping = 1.0f;
+
+    [Range (0.0f, 100.0f)]
+    public float m_Frequency = 5.0f;
+
+    public bool m_DrawDragLine = true;
+    public Color m_Color = Color.cyan;
+
+    private TargetJoint2D m_TargetJoint;
+
+    void Update ()
+    {
+        var worldPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+        if (Input.GetMouseButtonDown (0))
+        {
+            var collider = Physics2D.OverlapPoint (worldPos, m_DragLayers);
+            if (!collider)
+                return;
+
+            var body = collider.attachedRigidbody;
+            if (!body)
+                return;
+
+            m_TargetJoint = body.gameObject.AddComponent<TargetJoint2D> ();
+            m_TargetJoint.dampingRatio = m_Damping;
+            m_TargetJoint.frequency = m_Frequency;
+
+            m_TargetJoint.anchor = m_TargetJoint.transform.InverseTransformPoint (worldPos);
+        }
+        else if (Input.GetMouseButtonUp (0))
+        {
+            Destroy (m_TargetJoint);
+            m_TargetJoint = null;
+            return;
+        }
+
+        if (m_TargetJoint)
+        {
+            m_TargetJoint.target = worldPos;
+
+            if (m_DrawDragLine)
+                Debug.DrawLine (m_TargetJoint.transform.TransformPoint (m_TargetJoint.anchor), worldPos, m_Color);
+        }
+    }
+}
