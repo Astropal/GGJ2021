67626b87b08e96d5f231b5638cb841a7e2c45dd3
BaptisteMiq <baptiste.miquel@viacesi.fr>
2021-01-31 04:48:58 +0100

    Corrections mini game

--- Assets/Scripts/GlaceGame.cs
+++ Assets/Scripts/GlaceGame.cs
@@ -6,41 +6,87 @@ using UnityEngine.SceneManagement;
 public class GlaceGame : MonoBehaviour
 {
     public int nbIngredients = 0;
+
     private int nbMg = 0;
+
     private int nbHgO = 0;
+
+    public GameObject reservoir;
+
     public GameObject indicator;
+
     public GameObject indicatorSize;
 
     public GameObject potion_Mg;
+
     public GameObject potion_HgO;
 
+    public float initY;
+
     private bool lost = false;
+    private bool win = false;
+
+    private int timeBeforeLost = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        initY = indicatorSize.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(lost) {
-            if(indicatorSize.transform.localScale.y > 0) {
-                indicatorSize.transform.localScale = new Vector2(indicatorSize.transform.localScale.x, indicatorSize.transform.localScale.y - 0.002f);
-            } else {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+        if (lost)
+        {
+            if (timeBeforeLost > 0)
+            {
+                timeBeforeLost--;
+                this.resize(0.41f * nbIngredients);
             }
-        } else {
-            indicatorSize.transform.localScale = new Vector2(indicatorSize.transform.localScale.x, 0.2f * nbIngredients);
+            else
+            {
+                if (indicatorSize.transform.localScale.y > 0)
+                {
+                    this.resize(indicatorSize.transform.localScale.y - 0.002f);
+                }
+                else
+                {
+                    SceneManager
+                        .LoadScene(SceneManager.GetActiveScene().buildIndex,
+                        LoadSceneMode.Single);
+                }
+            }
         }
+        else if(!win)
+        {
+            this.resize(0.41f * nbIngredients);
+        } else {
+            // Win
+
+        }
+    }
+
+    void resize(float amount)
+    {
+        indicatorSize.transform.position =
+            new Vector3(indicatorSize.transform.position.x,
+                initY + (amount / 2),
+                indicatorSize.transform.position.z);
+        indicatorSize.transform.localScale =
+            new Vector3(indicatorSize.transform.localScale.x,
+                amount,
+                indicatorSize.transform.localScale.z);
     }
 
     void OnTriggerEnter2D(Collider2D Col)
     {
-       if(Col.gameObject.tag == "LiquideGlace" && nbIngredients < 3) {
-           nbIngredients++;
+        if (Col.gameObject.tag == "LiquideGlace" && nbIngredients < 3)
+        {
+            nbIngredients++;
 
-           if(Col.gameObject.GetComponent<Type>().type == "Mg") {
+            if (Col.gameObject.GetComponent<Type>().type == "Mg")
+            {
                 Color newColor;
                 SpriteRenderer sr = indicator.GetComponent<SpriteRenderer>();
                 newColor = sr.color;
@@ -48,9 +94,10 @@ public class GlaceGame : MonoBehaviour
                 newColor.g *= 0.7f;
                 sr.color = newColor;
                 nbMg++;
-           }
+            }
 
-           if(Col.gameObject.GetComponent<Type>().type == "HgO") {
+            if (Col.gameObject.GetComponent<Type>().type == "HgO")
+            {
                 Color newColor;
                 SpriteRenderer sr = indicator.GetComponent<SpriteRenderer>();
                 newColor = sr.color;
@@ -58,19 +105,21 @@ public class GlaceGame : MonoBehaviour
                 newColor.g *= 1.2f;
                 sr.color = newColor;
                 nbHgO++;
-           }
+            }
 
-           if(Col.gameObject.GetComponent<Type>().type == "Astro") {
+            if (Col.gameObject.GetComponent<Type>().type == "Astro")
+            {
                 Color newColor;
                 SpriteRenderer sr = indicator.GetComponent<SpriteRenderer>();
                 newColor = sr.color;
                 sr.color = new Color(255, 0, 0, 1);
-                nbIngredients = 4;
-                indicatorSize.transform.localScale = new Vector2(indicatorSize.transform.localScale.x, 0.2f * nbIngredients);
+                nbIngredients = 3;
+                this.resize(0.41f * nbIngredients);
                 lost = true;
-           }
+            }
 
-           if(Col.gameObject.GetComponent<Type>().type == "Orange") {
+            if (Col.gameObject.GetComponent<Type>().type == "Orange")
+            {
                 Color newColor;
                 SpriteRenderer sr = indicator.GetComponent<SpriteRenderer>();
                 newColor = sr.color;
@@ -78,9 +127,10 @@ public class GlaceGame : MonoBehaviour
                 newColor.g *= 1.2f;
                 newColor.b *= 0.2f;
                 sr.color = newColor;
-           }
+            }
 
-           if(Col.gameObject.GetComponent<Type>().type == "Vert") {
+            if (Col.gameObject.GetComponent<Type>().type == "Vert")
+            {
                 Color newColor;
                 SpriteRenderer sr = indicator.GetComponent<SpriteRenderer>();
                 newColor = sr.color;
@@ -88,9 +138,10 @@ public class GlaceGame : MonoBehaviour
                 newColor.g *= 2f;
                 newColor.b *= 0.2f;
                 sr.color = newColor;
-           }
+            }
 
-           if(Col.gameObject.GetComponent<Type>().type == "Violet") {
+            if (Col.gameObject.GetComponent<Type>().type == "Violet")
+            {
                 Color newColor;
                 SpriteRenderer sr = indicator.GetComponent<SpriteRenderer>();
                 newColor = sr.color;
@@ -98,28 +149,36 @@ public class GlaceGame : MonoBehaviour
                 newColor.g *= 0.2f;
                 newColor.b *= 2f;
                 sr.color = newColor;
-           }
+            }
 
-           Debug.Log(nbIngredients);
-           Destroy(Col.gameObject);
+            this.resize(0.41f * nbIngredients);
 
-           if(nbIngredients == 3) {
-               if(nbMg == 1 && nbHgO == 2) {
-                   Debug.Log("Gagné!");
-               } else {
-                   Debug.Log("Perdu!");
-                   lost = true;
-               }
-           }
-       }
-       else {
-           Debug.Log(nbIngredients);
-       }
+            Debug.Log (nbIngredients);
+            Destroy(Col.gameObject);
 
-       if(nbIngredients == 2)
-       {
-           Debug.Log("Gagné !");
-       }
+            if (nbIngredients == 3)
+            {
+                if (nbMg == 1 && nbHgO == 2)
+                {
+                    Debug.Log("Gagné!");
+                    SpriteRenderer sr = indicator.GetComponent<SpriteRenderer>();
+                    sr.color = new Color(0.6f, 0.1f, 0.1f, 1.0f);
+                    reservoir.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                    Rigidbody2D rb = reservoir.transform.GetComponent<Rigidbody2D>();
+                    rb.AddForce(new Vector2(-3e4f, 5e4f));
+                    reservoir.transform.transform.Rotate(0, 0, 10);
+                    win = true;
+                }
+                else
+                {
+                    Debug.Log("Perdu!");
+                    lost = true;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log (nbIngredients);
+        }
     }
-
 }
---
+++ Assets/Scripts/Thermo/Thermo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thermo : MonoBehaviour
+{
+  public GameObject indicatorSize;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  private int elements = 0;
+
+  private void OnTriggerEnter2D(Collider2D Col)
+  {
+    if (Col.gameObject.tag == "Reservoir")
+    {
+      elements++;
+      Col.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+      GameObject objTemplate = GameObject.FindWithTag("Full_Thermo").transform.Find("Reservoir").gameObject;
+      Col.gameObject.transform.position = objTemplate.transform.position;
+      Rigidbody2D rb = Col.gameObject.transform.GetComponent<Rigidbody2D>();
+      rb.bodyType = RigidbodyType2D.Static;
+    }
+    if (Col.gameObject.tag == "Reglette")
+    {
+      elements++;
+      Col.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+      GameObject objTemplate = GameObject.FindWithTag("Full_Thermo").transform.Find("Reglette").gameObject;
+      Col.gameObject.transform.position = objTemplate.transform.position;
+      Rigidbody2D rb = Col.gameObject.transform.GetComponent<Rigidbody2D>();
+      rb.bodyType = RigidbodyType2D.Static;
+    }
+    if (Col.gameObject.tag == "Tube")
+    {
+      elements++;
+      Col.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+      GameObject objTemplate = GameObject.FindWithTag("Full_Thermo").transform.Find("Tube").gameObject;
+      Col.gameObject.transform.position = objTemplate.transform.position;
+      Rigidbody2D rb = Col.gameObject.transform.GetComponent<Rigidbody2D>();
+      rb.bodyType = RigidbodyType2D.Static;
+    }
+    if (elements == 3)
+    {
+      Debug.Log("Thermomètre terminé!");
+      indicatorSize.transform.localScale =
+            new Vector3(indicatorSize.transform.localScale.x,
+                4.2f,
+                indicatorSize.transform.localScale.z);
+    }
+  }
+}
